0acd212fa7eaafaf3087f91e888c10789a91b626
Siemen Baader <siemenbaader@gmail.com>
2012-03-27 14:57:32 +0200

    Integrated dav server

---
+++ dav-server.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var jsDAV = require("jsDAV/lib/jsdav"),
+    jsDAV_Locks_Backend_FS = require("jsDAV/lib/DAV/plugins/locks/fs");
+
+jsDAV.debugMode = true;
+
+jsDAV.createServer({
+    node: __dirname,
+    locksBackend: new jsDAV_Locks_Backend_FS(__dirname + "/assets")
+}, 8000);
